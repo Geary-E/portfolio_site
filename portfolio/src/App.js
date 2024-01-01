ce06c76b7b1c2826f871439fe44e717fbda0dfe9
Geary-E <geary16.erua@gmail.com>
2023-12-31 19:32:04 -0600

    Portfolio Updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,39 @@
 
 import './App.css';
+import React from 'react';
+import { Link } from 'react-scroll';
 import bgImage from './images/image1.png';
+import profileImage from './images/image2.JPG';
+import { useRef } from 'react';
+//import ScrollButton from './ScrollButton';
 import Typewriter from 'typewriter-effect';
 
 function App() {
+  
+  const about = useRef(null);
+  const projects = useRef(null);
+  const skills = useRef(null);
+  const contact = useRef(null);
+
+  const scroll = (elementRef => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: 'smooth',
+    });
+  });
+
   return (
     <div className="App">
       
       <div className="nav-bar">
         <ul className="navbar-list">
-          <li><a href="#">About</a></li>
-          <li><a href="#">Projects</a></li>
-          <li><a href="#">Skills</a></li>
-          <li><a href="#">Contact</a></li>
+          <li onClick={() => scroll(about)}>About</li>
+          <li onClick={() => scroll(projects)}> Projects</li>
+          <li onClick={() => scroll(skills)}> Skills </li>
+          <li onClick={() => scroll(contact)}> Contact </li>
         </ul>
       </div>
+
       <div className="main-container">
         <img className="home-image" src={bgImage} />
         <div className="typewriter">
@@ -25,7 +44,105 @@ function App() {
           loop: true,
   }}
 /></div>
-      </div>
+      </div><br/><br/>
+
+      <div class="sections">
+        
+        <div ref={about} className="first-section">
+         <h1> About </h1>
+            <div className="about-section">
+              <div className="left-side">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Placerat in egestas erat imperdiet sed euismod nisi. 
+                Ipsum suspendisse ultrices gravida dictum fusce. 
+                Nunc mattis enim ut tellus elementum sagittis vitae et.
+                  Suspendisse in est ante in nibh mauris cursus mattis molestie. </p>
+                <p>
+                Ut faucibus pulvinar elementum integer enim neque volutpat. 
+                Eu scelerisque felis imperdiet proin. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim.
+                 Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi. Pharetra magna ac placerat vestibulum.
+                Sit amet volutpat consequat mauris. Rhoncus est pellentesque elit ullamcorper. Orci sagittis eu volutpat odio
+                 facilisis mauris sit amet. Facilisis sed odio morbi quis. Malesuada pellentesque elit eget gravida cum. 
+                 Vitae ultricies leo integer malesuada nunc vel risus commodo. 
+                 </p>
+                 <p>
+                 Tellus orci ac auctor augue mauris augue 
+                 neque gravida in. Nec feugiat nisl pretium fusce id velit. 
+                 Diam vel quam elementum pulvinar etiam non quam. Eleifend mi in nulla posuere 
+                 sollicitudin aliquam. Lobortis elementum nibh tellus molestie nunc non blandit massa enim. 
+                 Aliquam nulla facilisi cras fermentum. Enim nulla aliquet porttitor lacus luctus accumsan tortor. Tincidunt id aliquet risus feugiat. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
+                 Elit pellentesque habitant morbi tristique senectus. Sit amet mattis vulputate enim nulla aliquet porttitor.
+                 Lobortis mattis aliquam faucibus purus in. Vitae sapien pellentesque habitant morbi tristique senectus et netus. 
+                 Porttitor eget dolor morbi non arcu risus quis. Egestas diam in arcu cursus euismod quis viverra nibh cras. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Etiam erat velit scelerisque in dictum non consectetur a. Morbi tempus iaculis urna id volutpat lacus laoreet non. Gravida arcu ac tortor dignissim convallis aenean. Suspendisse in est ante in nibh mauris cursus mattis. 
+                 Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Montes nascetur ridiculus mus mauris vitae ultricies leo.
+              </p>
+              </div>
+              <div className="right-side">
+              <img src={profileImage} />
+              </div>
+              </div>
+          </div><br/>
+
+          <div ref={projects} className="second-section">
+            <h1> Projects </h1>
+            <div className="project-section">
+              <div className="flex-item">
+              <h2> Snake </h2>
+              </div>
+
+              <div className="flex-item">
+              <h2> Rock-Paper-Scissors </h2>
+              </div>
+
+              <div className="flex-item">
+              <h2>Accessibility Portal</h2>
+              </div>
+
+              <div className="flex-item">
+              <h2> Roman Numeral Converter</h2>
+              </div>
+              </div>
+          </div>
+
+          <div ref={skills} className="third-section">
+              <h1> Skills</h1>
+              <div className="skills-section">
+
+              <div className="flex-item">  
+              <h2> Problem Solving </h2>
+              </div>
+
+              <div className="flex-item">
+              <h2> Technical </h2>
+              </div>
+
+              <div className="flex-item">
+              <h2> Communication </h2>
+              </div>
+
+              <div className="flex-item">
+              <h2> Teamwork </h2>
+              </div>
+
+              <div className="flex-item">
+              <h2> Website Design </h2>
+              </div> 
+
+              </div>
+          </div>
+
+          <div ref={contact} className="contact-section">
+              <h1> Contact </h1>
+              
+              <div className="contact-items">
+                <h2> Email </h2>
+                <h2> LinkedIn </h2>
+              </div>
+          </div>
+
+          </div>
     </div>
   );
 }
