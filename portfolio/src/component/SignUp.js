06262a919da9e50e134a32f7fb54976c15f2bd65
Geary-E <geary16.erua@gmail.com>
2025-01-10 19:02:27 -0600

    POST request for user in UI update

--- portfolio/src/component/SignUp.js
+++ portfolio/src/component/SignUp.js
@@ -7,24 +7,41 @@ import '../SignUp.css';
 const SignUp = () => {
 
     const { isDarkMode } = useContext(DarkModeContext); // dark mode
-    //const [data, setData] = useState([]);   // useState for data
+    
+    const [data, setData] = useState({
+        email: "",
+        user_name: "",
+        password: ""
+    });   // useState for data
 
-    //const submit = () => {
+   const handleInput = (event) => {
+        setData({...data, [event.target.name]: event.target.value})
+   }
 
-      //  axios.post()
-    //}
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    //console.log(data);  //testing purposes
+    axios.post("https://blog-section2-301885cf5d53.herokuapp.com/api/users/", data)
+    .then(response => {
+        console.log(response);
+        alert("User created!");
+   })
+    .catch(err => console.log(err));
+   }
 
     return (
         <div className={`signup-page ${isDarkMode ? 'dark mode' : 'light mode '}`}>
             <div className='signup-section'>
-                <h1> Create Your Account </h1>
-                <label>Email </label><br/><br/>
-                <input className="name-signup" name="email" placeholder="Email" /><br/><br/>
-                <label>Username </label><br/><br/>
-                <input className="user-signup" name="username" placeholder="Username" /><br/><br/>
-                <label>Password</label><br/><br/>
-                <input className="pass-signup" name="password" placeholder="Password" /><br/><br/><br/>
-                <button className={`signup-submit ${isDarkMode ? 'dark' : 'light'}`}> Create Account </button>
+                <form onSubmit={handleSubmit} method="POST">
+                    <h1> Create Your Account </h1>
+                    <label>Email </label><br/><br/>
+                    <input className="name-signup" name="email" onChange={handleInput}  placeholder="Email" /><br/><br/>
+                    <label>Username </label><br/><br/>
+                    <input className="user-signup" name="user_name" onChange={handleInput}  placeholder="Username" /><br/><br/>
+                    <label>Password</label><br/><br/>
+                    <input className="pass-signup" name="password" onChange={handleInput}  placeholder="Password" /><br/><br/><br/>
+                    <button className={`signup-submit ${isDarkMode ? 'dark' : 'light'}`}> Create Account </button>
+                 </form>   
             </div>
 
         </div>
